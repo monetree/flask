9233ab9f0943a7a19f83b314b2d167646fe6f8d7
monetree <soubhagyakumar666@gmail.com>
2019-01-19 19:03:10 +0530

    adding changes

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
-from flask import Flask, request
-from flask_restful import Resource, Api
-from flask_jwt import JWT, jwt_required
+from flask import Flask
+from flask_restful import Api
+from flask_jwt import JWT
 from security import authenticate, identity
-
+from user import UserRegister
+from item import Item, ItemList
 
 app = Flask(__name__)
 app.secret_key = 'Thinkonce'
@@ -10,30 +11,12 @@ api = Api(app)
 
 jwt = JWT(app, authenticate, identity)
 
-items = []
-
-class Item(Resource):
-    @jwt_required()
-    def get(self, name):
-        item = next(filter(lambda x: x['name'] == name, items), None)
-        return {"item": item}, 200 if item else 404
-
-    def post(self, name):
-        if next(filter(lambda x: x['name'] == name, items), None):
-            return {"message":"An item with name {} is already exist.".format(name)}
-        data = request.get_json()
-        item = {"name": name, "price": data["price"]}
-        items.append(item)
-        return item, 201
-
-
-class ItemList(Resource):
-    def get(self):
-        return {'items': items}
-
 
 api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
+api.add_resource(UserRegister, '/register')
 
 
-app.run(port=5000, debug=True)
+
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
